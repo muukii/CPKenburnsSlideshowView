ffb743bc4d6de737913e56576c099bd99727c564
muukii <muukii.muukii@gmail.com>
2014-04-13 22:27:31 +0900

    Fix

--- CPKenBurnsSlideshowView/CPKenBurnsSlideshowView.m
+++ CPKenBurnsSlideshowView/CPKenBurnsSlideshowView.m
@@ -229,6 +229,9 @@ typedef NS_ENUM(NSInteger, CPKenBurnsSlideshowViewOrder) {
 
 - (void)scrollToNextPhoto
 {
+    [[self previousKenBurnsView] setAlpha:1];
+    [[self nextKenBurnsView] setAlpha:0];
+
     CGPoint currentOffset = self.scrollView.contentOffset;
     currentOffset.x += CGRectGetWidth(self.scrollView.bounds);
     self.scrollView.fadeDuration = self.automaticFadeDuration;
@@ -237,6 +240,9 @@ typedef NS_ENUM(NSInteger, CPKenBurnsSlideshowViewOrder) {
 
 - (void)scrollToPreviousPhoto
 {
+    [[self previousKenBurnsView] setAlpha:0];
+    [[self nextKenBurnsView] setAlpha:1];
+    
     CGPoint currentOffset = self.scrollView.contentOffset;
     currentOffset.x -= CGRectGetWidth(self.scrollView.bounds);
     self.scrollView.fadeDuration = self.automaticFadeDuration;
